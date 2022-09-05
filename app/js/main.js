1c5b41715053c3e371f6a8dfb0effeab6948e4b6
Rizvan <xl92@mail.ru>
2022-09-05 20:39:03 +0300

    finish header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,43 @@
-window.addEventListener('DOMContentLoaded', function() {
-    'use strict';/* JS сработает когда  DOM дерево подгрузится РЕКОМЕНДУЕТСЯ*/
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    //
+    const body = document.querySelector('body');
+    //header
+    body.insertAdjacentHTML('afterbegin', '<header> <div> <div> <h1></h1><a></a> </div> </div> </header>');
+    const header = document.querySelector('header'),
+        container = header.querySelector('div'),
+        headerBox = container.querySelector('div'),
+        headerTitle = headerBox.querySelector('h1'),
+        btn = headerBox.querySelector('a');
+
+    header.classList.add('header');
+    container.classList.add('container');
+    headerBox.classList.add('header__box');
+    headerTitle.classList.add('header__title');
+    btn.classList.add('btn');
+
+    headerTitle.insertAdjacentText('afterbegin', 'САНТЕХНИК ГРОЗНЫЙ');
+    btn.insertAdjacentText('afterbegin', '111222333');
+    btn.setAttribute('href', '#');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
 })
