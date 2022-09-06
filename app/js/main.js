6a68f6f33069b64f5ee8908134b1404697c33125
Rizvan <xl92@mail.ru>
2022-09-06 16:22:36 +0300

    finish services section

--- app/js/main.js
+++ app/js/main.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //services
     const fonImg = document.querySelector('section.fonImg');
-    fonImg.insertAdjacentHTML('afterend', '<section> <div> <h2></h2><div> <div> <ul> <li></li><li></li><li></li><li></li> </ul> </div><div> <ul></ul> </div> </div><div></div> </div> </section>');
+    fonImg.insertAdjacentHTML('afterend', '<section> <div> <h2></h2><div> <div> <ul> <li></li><li></li><li></li><li></li> </ul> </div><div> <ul> <li></li><li></li><li></li><li></li> </ul> </div> </div><div></div> </div> </section>');
     const section = document.querySelectorAll('section');
 
     let [, services] = section;//Деструктуризация
@@ -40,48 +40,38 @@ window.addEventListener('DOMContentLoaded', function () {
         servicesBox = servicesContainer.querySelector('div'),
         servicesLeft = servicesBox.querySelector('div'),
         ulLiItem = servicesLeft.querySelectorAll('li'),
-        servicesRight = servicesBox.querySelectorAll('div');
+        servicesRight = servicesBox.querySelectorAll('div'),
+        servicesDscr = servicesContainer.lastElementChild;
 
     let [, right] = servicesRight;//Деструктуризация
+    const ulLiItemR = right.querySelectorAll('li');
 
     services.classList.add('services');
     servicesContainer.classList.add('container');
     servicesTitle.classList.add('services__title');
     servicesBox.classList.add('services__box');
     servicesLeft.classList.add('services__left');
+    right.classList.add('services__right');
     ulLiItem.forEach(li => {
         li.classList.add('item');
-    })
-    right.classList.add('services__right');
+    });
+    ulLiItemR.forEach(li => {
+        li.classList.add('item');
+    });
+    servicesDscr.classList.add('services__dscr');
 
-    /*
-        <section class="services">
-            <div class="container">
-                <h2 class="services__title"></h2>
-                <div class="services__box">
-                    <div class="services__left">
-                        <ul>
-                            <li class="item"></li>
-                            <li class="item"></li>
-                            <li class="item"></li>
-                            <li class="item"></li>
-                        </ul>
-                    </div>
-                    <div class="services__right">
-                        <ul>
-                            <li class="item"></li>
-                            <li class="item"></li>
-                            <li class="item"></li>
-                            <li class="item"></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="services__dscr"></div>
-            </div>
-        </section> 
-     */
-
-
+    servicesTitle.insertAdjacentText('afterbegin', 'НАШИ УСЛУГИ:');
+    let [li1, li2, li3, li4] = ulLiItem,
+        [li5, li6, li7, li8] = ulLiItemR;
+    li1.insertAdjacentText('afterbegin', 'УСТРАНЕНИЕ ЗАСОРОВ');
+    li2.insertAdjacentText('afterbegin', 'УСТРАНЕНИЕ ПРОТЕЧКИ');
+    li3.insertAdjacentText('afterbegin', 'РЕМОНТ САНТЕХНИКИ');
+    li4.insertAdjacentText('afterbegin', 'УСТАНОВКА ВОДОСЧЕТЧИКОВ');
+    li5.insertAdjacentText('afterbegin', 'ЗАМЕНА И РЕМОНТ ТРУБ');
+    li6.insertAdjacentText('afterbegin', 'УСТАНОВКА ТЕХНИКИ');
+    li7.insertAdjacentText('afterbegin', 'УСТАНОВКА ВАННЫ');
+    li8.insertAdjacentText('afterbegin', 'И МНОГОЕ ДРУГОЕ...');
+    servicesDscr.insertAdjacentText('afterbegin', 'Все виды сантехнических услуг, позвоните нам, чтобы уточнить нужную Вам услугу! Пенсионерам и инвалидам скидка 10% на все услуги.');
 
 
 
